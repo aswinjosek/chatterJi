16e46f507e1f770bbe8b90e7bfde7b3a4a02169a
Aswin Jose <aswin.jose@hrblock.com>
2023-08-19 12:58:26 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'catterji'`, () => {
+  it(`should have as title 'chatterji'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('catterji');
+    expect(app.title).toEqual('chatterji');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('catterji app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('chatterji app is running!');
   });
 });
